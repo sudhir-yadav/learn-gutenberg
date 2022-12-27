820576d4ffc3c4f4c86dcbac2734d0c5f9a9ffa7
Sudhir Yadav <sudhiry875@gmail.com>
2022-12-27 17:22:51 +0530

    Register category for blocks

--- build/static-block/index.asset.php
+++ build/static-block/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => '347b026d4d6f5d83144f');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => '5423b8f530c271e91ac5');
--- learn-gutenberg.php
+++ learn-gutenberg.php
@@ -15,6 +15,30 @@
  */
 
 /**
+ * Filters the default array of categories for block types to add custom category.
+ *
+ * @see https://developer.wordpress.org/reference/hooks/block_categories_all/
+ *
+ * @param array[]                 $block_categories     Array of categories for block types.
+ * @param WP_Block_Editor_Context $block_editor_context The current block editor context.
+ */
+function lg_block_categories_all_filter( $block_categories, $block_editor_context ) {
+
+	return array_merge(
+		$block_categories,
+		[
+			[
+				'slug'  => 'learn-gutenberg',
+				'title' => esc_html__( 'Learn Gutenberg', 'learn-gutenberg' ),
+				'icon'  => null,
+			],
+		]
+	);
+
+}
+add_filter( 'block_categories_all', 'lg_block_categories_all_filter', 10, 2 ); 
+
+/**
  * Registering static block.
  */
 function lg_static_block_init() {
